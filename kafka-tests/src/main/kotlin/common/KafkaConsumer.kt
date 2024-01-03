fed38cfaba755888efb83f677ee213ad44223767
giacomoaccursi <giacomo.accursi@gmail.com>
2024-01-03 18:10:31 +0100

    feat: create simple kafka consumer

---
+++ kafka-tests/src/main/kotlin/common/KafkaConsumer.kt
@@ -0,0 +1,26 @@
+package common
+
+import org.apache.kafka.clients.admin.NewTopic
+import org.apache.kafka.clients.consumer.ConsumerRecords
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import java.time.Duration
+import java.util.*
+
+class Consumer(properties: Properties, private val topic: NewTopic) {
+    private val consumer: KafkaConsumer<String, String> = KafkaConsumer<String, String>(properties).also {
+        it.subscribe(listOf(topic.name()))
+    }
+
+    fun init() {
+        while (true) {
+            val records: ConsumerRecords<String, String> = consumer.poll(Duration.ofMillis(1000))
+            records.forEach { record ->
+                println("Consumed: ${record.value()}")
+            }
+        }
+    }
+
+    fun close() {
+        consumer.close()
+    }
+}
