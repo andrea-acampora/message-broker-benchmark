c1fc064c3245ba1142f4393d64bea4e7672dd3a0
giacomoaccursi <giacomo.accursi@gmail.com>
2024-01-12 09:17:57 +0100

    chore: remove topic config

--- kafka-driver/src/main/kotlin/common/KafkaConfig.kt
+++ kafka-driver/src/main/kotlin/common/KafkaConfig.kt
@@ -2,7 +2,6 @@ package common
 
 data class KafkaConfig(
     val replicationFactor: Short,
-    val topicConfig: String,
     val commonConfig: String,
     val producerConfig: String,
     val consumerConfig: String
