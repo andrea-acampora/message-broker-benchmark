bc09d1c7606b1b787dfe97b0f9384b84157001be
giacomoaccursi <giacomo.accursi@gmail.com>
2024-01-15 09:49:42 +0100

    chore: send 1kb instead 1mb of message

--- benchmark/src/main/kotlin/TestThroughput.kt
+++ benchmark/src/main/kotlin/TestThroughput.kt
@@ -1,16 +1,13 @@
 class TestThroughput(val producer: BenchmarkProducer<ByteArray>, private val testDuration: Long) {
     fun runTest() {
         val startTime = System.currentTimeMillis()
-
         while (System.currentTimeMillis() - startTime < testDuration) {
-            producer.send(ByteArray(1024*1024))
+            producer.send(ByteArray(1024), false)
         }
     }
 
     fun collectResult(): Double {
-
-        val throughput: Double = producer.timeList.size.toDouble() / (testDuration.toDouble()/1000)
-
+        val throughput: Double = (producer.timeList.size.toDouble() / 1024) / (testDuration.toDouble() / 1000)
         return throughput
     }
 }
