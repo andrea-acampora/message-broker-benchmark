d8b54746c56ddf97c85be6449e74030aa84aa327
giacomoaccursi <giacomo.accursi@gmail.com>
2024-01-15 09:50:04 +0100

    chore: close kafka consumer and producer after test

--- src/main/kotlin/LatencyTest.kt
+++ src/main/kotlin/LatencyTest.kt
@@ -6,28 +6,30 @@ fun main() {
     val kafkaLatency = testKafkaLatency()
     val pulsarLatency = testPulsarLatency()
     val rabbitMQLatency = testRabbitMQLatency()
-    print(kafkaLatency)
+    println(kafkaLatency)
     println(pulsarLatency)
     println(rabbitMQLatency)
 }
 
 fun testKafkaLatency(): List<Long> {
-    val kafkaLoader = KafkaLoader("/kafka.yml", "topic-1")
+    val kafkaLoader = KafkaLoader("/kafka.yml", "topic-2")
     val kafkaBenchmarkLatency = TestLatency(kafkaLoader.producer, kafkaLoader.consumer)
-    kafkaBenchmarkLatency.runTest(50)
+    kafkaBenchmarkLatency.runTest(20)
+    kafkaLoader.consumer.close()
+    kafkaLoader.producer.close()
     return kafkaBenchmarkLatency.collectResult()
 }
 
 fun testPulsarLatency(): List<Long> {
     val pulsarLoader = PulsarLoader("/pulsar.yml", "topic-1")
     val pulsarBenchmarkLatency = TestLatency(pulsarLoader.producer, pulsarLoader.consumer)
-    pulsarBenchmarkLatency.runTest(10)
+    pulsarBenchmarkLatency.runTest(20)
     return pulsarBenchmarkLatency.collectResult()
 }
 
 fun testRabbitMQLatency(): List<Long> {
     val rabbitMqLoader = RabbitMqLoader("queue-1")
     val rabbitMQBenchmarkLatency = TestLatency(rabbitMqLoader.producer, rabbitMqLoader.consumer)
-    rabbitMQBenchmarkLatency.runTest(50)
+    rabbitMQBenchmarkLatency.runTest(20)
     return rabbitMQBenchmarkLatency.collectResult()
 }
