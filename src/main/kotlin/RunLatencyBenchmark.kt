fb076dec60adfebf1d44d602f576d475858bc80c
Andrea Acampora <andrea.acampora@studio.unibo.it>
2024-01-16 18:17:22 +0100

    chore: update tests with dedicated classes

---
+++ src/main/kotlin/RunLatencyBenchmark.kt
@@ -0,0 +1,40 @@
+import common.KafkaLoader
+import common.PulsarLoader
+import common.RabbitMQLoader
+import consumer.BenchmarkConsumer
+import producer.BenchmarkProducer
+
+/** The number of messages sent in the Latency Benchmark for each broker. */
+const val MESSAGE_COUNT = 5000
+
+/**
+ * Run a Latency Benchmark for all Message Brokers.
+ */
+fun main() {
+    testKafkaLatency(MESSAGE_COUNT)
+    testPulsarLatency(MESSAGE_COUNT)
+    testRabbitMQLatency(MESSAGE_COUNT)
+}
+
+private fun testLatency(producer: BenchmarkProducer<ByteArray>, consumer: BenchmarkConsumer, messageCount: Int) =
+    LatencyBenchmark(producer, consumer, messageCount).let { benchmark ->
+        benchmark.runTest()
+        producer.close()
+        consumer.close()
+        println(benchmark.collectResult())
+    }
+
+private fun testKafkaLatency(messageCount: Int) =
+    with(KafkaLoader("/kafka.yml", "topic-1")) {
+        testLatency(this.producer, this.consumer, messageCount)
+    }
+
+private fun testPulsarLatency(messageCount: Int) =
+    with(PulsarLoader("/pulsar.yml", "topic-1")) {
+        testLatency(this.producer, this.consumer, messageCount)
+    }
+
+private fun testRabbitMQLatency(messageCount: Int) =
+    with(RabbitMQLoader("queue-1")) {
+        testLatency(this.producer, this.consumer, messageCount)
+    }
---
+++ src/main/kotlin/RunThroughputBenchmark.kt
@@ -0,0 +1,38 @@
+import common.KafkaLoader
+import common.PulsarLoader
+import common.RabbitMQLoader
+import producer.BenchmarkProducer
+
+/** The duration of the node failure benchmark for each Broker. */
+const val DURATION = 20000L
+
+/**
+ * Run a Throughput Benchmark for all Message Brokers.
+ */
+fun main() {
+    testKafkaThroughput(DURATION)
+    testPulsarThroughput(DURATION)
+    testRabbitMQThroughput(DURATION)
+}
+
+private fun testThroughput(producer: BenchmarkProducer<ByteArray>, duration: Long): Double =
+    ThroughputBenchmark(producer, duration).let { benchmark ->
+        benchmark.runTest()
+        producer.close()
+        return benchmark.collectResult()
+    }
+
+private fun testKafkaThroughput(duration: Long) =
+    with(KafkaLoader("/kafka.yml", "topic-1")) {
+        testThroughput(this.producer, duration)
+    }
+
+private fun testPulsarThroughput(duration: Long) =
+    with(PulsarLoader("/pulsar.yml", "topic-1")) {
+        testThroughput(this.producer, duration)
+    }
+
+private fun testRabbitMQThroughput(duration: Long) =
+    with(RabbitMQLoader("queue-1")) {
+        testThroughput(this.producer, duration)
+    }
