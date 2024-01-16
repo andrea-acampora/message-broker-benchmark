21da46f2da7801618e5a5574c1df4c7544b88b78
Andrea Acampora <andrea.acampora@studio.unibo.it>
2024-01-16 18:15:37 +0100

    style: add some comments

--- kafka-driver/src/main/kotlin/common/KafkaLoader.kt
+++ kafka-driver/src/main/kotlin/common/KafkaLoader.kt
@@ -16,44 +16,52 @@ import java.io.File
 import java.io.StringReader
 import java.util.Properties
 
+/**
+ * A Loader for a Kafka Producer and Kafka Consumer given a configuration file and a topic.
+ */
 class KafkaLoader(
     configurationFile: String,
-    topicName: String
+    topicName: String,
 ) {
 
+    /** The Kafka Producer. */
     lateinit var producer: KafkaProducer
+
+    /** The Kafka Consumer. */
     lateinit var consumer: KafkaConsumer
 
     init {
         object {}.javaClass.getResource(configurationFile)?.let {
+
             val config: KafkaConfig = ObjectMapper(YAMLFactory()).registerKotlinModule().readValue(File(it.toURI()))
             val commonProperties = Properties()
-            commonProperties.load(StringReader(config.commonConfig))
             val consumerProperties = Properties()
+            val producerProperties = Properties()
+
+            commonProperties.load(StringReader(config.commonConfig))
             commonProperties.forEach { key, value ->
                 consumerProperties[key] = value
-            }
-            consumerProperties[ConsumerConfig.GROUP_ID_CONFIG] = "test-group"
-            consumerProperties.load(StringReader(config.consumerConfig))
-            consumerProperties[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java.name
-            consumerProperties[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = ByteArrayDeserializer::class.java.name
-            val producerProperties = Properties()
-            commonProperties.forEach { key, value ->
                 producerProperties[key] = value
             }
+            consumerProperties.load(StringReader(config.consumerConfig))
+            consumerProperties[ConsumerConfig.GROUP_ID_CONFIG] = "test-group"
+            consumerProperties[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java.name
+            consumerProperties[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = ByteArrayDeserializer::class.java.name
+
             producerProperties.load(StringReader(config.producerConfig))
             producerProperties[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java.name
             producerProperties[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = ByteArraySerializer::class.java.name
 
             with(Admin.create(commonProperties)) {
-                if(this.listTopics().names().get().contains(topicName)) {
+                if (this.listTopics().names().get().contains(topicName)) {
                     this.deleteTopics(arrayListOf(topicName)).all().get()
                 }
                 this.createTopics(
-                    arrayListOf(NewTopic(topicName,  config.partitions, config.replicationFactor))
+                    arrayListOf(NewTopic(topicName, config.partitions, config.replicationFactor)),
                 ).values()[topicName]?.get()
                 this.close()
             }
+
             this.producer = KafkaProducer(producerProperties, topicName)
             this.consumer = KafkaConsumer(consumerProperties, topicName)
         }
--- pulsar-driver/src/main/kotlin/common/config/ProducerConfiguration.kt
+++ pulsar-driver/src/main/kotlin/common/config/ProducerConfiguration.kt
@@ -1,9 +1,20 @@
 package common.config
 
+/** The value of the batching max byes. */
+const val BATCHING_MAX_BYTES = 1024 * 1024
+
+/**
+ * The configuration file section for the Pulsar Producer.
+ * - [batchingEnabled]: value to enable the batch mode.
+ * - [batchingMaxPublishDelayMs]: the maximum batching delay.
+ * - [batchingMaxBytes]: the maximum batching bytes.
+ * - [blockIfQueueFull]: block the broker if the queue is full.
+ * - [pendingQueueSize]: the size of the pending queue.
+ */
 data class ProducerConfiguration(
     val batchingEnabled: Boolean = true,
     val batchingMaxPublishDelayMs: Long = 1L,
-    val batchingMaxBytes: Int = 1024 * 1024,
+    val batchingMaxBytes: Int = BATCHING_MAX_BYTES,
     val blockIfQueueFull: Boolean = true,
-    val pendingQueueSize: Int = 0
+    val pendingQueueSize: Int = 0,
 )
