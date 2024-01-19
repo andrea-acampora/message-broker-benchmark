317e26ffff3206c057598b850859dd0258e430ad
Andrea Acampora <andrea.acampora@studio.unibo.it>
2024-01-19 13:11:14 +0100

    test(pulsar): create tests for pulsar producer and consumer

---
+++ pulsar-driver/src/test/kotlin/PulsarTests.kt
@@ -0,0 +1,46 @@
+import common.PulsarLoader
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldNotBe
+import org.testcontainers.containers.PulsarContainer
+import org.testcontainers.utility.DockerImageName
+
+/**
+ * Testing Pulsar Producer and Pulsar Consumer using a Pulsar Container.
+ */
+class PulsarTests : StringSpec({
+
+    with(PulsarContainer(DockerImageName.parse("apachepulsar/pulsar:latest"))) {
+
+        this.start()
+        val loader = PulsarLoader("/pulsar.yml", "topic-test", this.pulsarBrokerUrl)
+
+        "It should be possible to create a Pulsar Producer" {
+            loader.producer shouldNotBe null
+        }
+
+        "Pulsar Producer should have an empty messages list at start" {
+            loader.producer.messagesTimestamp shouldHaveSize 0
+        }
+
+        "Pulsar Producer should be able to send a message" {
+            loader.producer.send("Testing producer....".encodeToByteArray(), true)
+            loader.producer.messagesTimestamp shouldHaveSize 1
+        }
+
+        "It should be possible to create a Pulsar Consumer" {
+            loader.consumer shouldNotBe null
+        }
+
+        "Pulsar Consumer should have an empty messages list at start" {
+            loader.consumer.messagesTimestamp shouldHaveSize 0
+        }
+
+        "Pulsar Consumer should be able to receive a message" {
+            loader.producer.send("Testing producer....".encodeToByteArray(), true)
+            loader.consumer.receive(true)
+            Thread.sleep(2000)
+            loader.consumer.messagesTimestamp shouldHaveSize 1
+        }
+    }
+})
--- pulsar-driver/src/test/kotlin/TestHelloWorld.kt
+++
@@ -1,9 +0,0 @@
-import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.shouldBe
-
-class TestHelloWorld : StringSpec({
-
-    "Sample test" {
-        "Hello World!" shouldBe "Hello World!"
-    }
-})
